99b9ac8f459ddc5bfe5e8deed751ae9c259efe23
andrew <uandrew1965@gmail.com>
2016-06-02 16:05:25 +0300

    .

---
+++ web-ui/src/main/scala/com/simplesys/container/SemiHandTraits/eakd_abonents_org_SemiHandTrait_Update.scala
@@ -0,0 +1,22 @@
+// This file is generated automatically (at 02.06.2016 16:03:48), do not spend any changes here, because they will be lost. Generator: "GenBOContainer, stage: #765"
+
+package ru.simplesys.defs.app.scala.container
+
+import com.simplesys.app.SessionContextSupport
+import com.simplesys.isc.system.ServletActorDyn
+import com.simplesys.isc.dataBinging.DSRequestDyn
+import com.simplesys.common.Strings._
+import ru.simplesys.defs.bo.eakd._
+
+trait eakd_abonents_org_SemiHandTrait_Update extends SessionContextSupport with ServletActorDyn {
+    
+/////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!!!! DON'T MOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///////////////////////////////    
+    val requestData = new DSRequestDyn(request)    
+    
+    logger debug s"Request for Update: ${newLine + requestData.toPrettyString}"    
+    
+    val dataSet = Abonents_orgDS(ds)    
+/////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!! END DON'T MOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///////////////////////////////    
+    
+
+}
---
+++ web-ui/src/main/scala/com/simplesys/container/SemiHandTraits/eakd_mvid_SemiHandTrait_Fetch.scala
@@ -0,0 +1,22 @@
+// This file is generated automatically (at 02.06.2016 16:03:48), do not spend any changes here, because they will be lost. Generator: "GenBOContainer, stage: #765"
+
+package ru.simplesys.defs.app.scala.container
+
+import com.simplesys.app.SessionContextSupport
+import com.simplesys.isc.system.ServletActorDyn
+import com.simplesys.isc.dataBinging.DSRequestDyn
+import com.simplesys.common.Strings._
+import ru.simplesys.defs.bo.eakd._
+
+trait eakd_mvid_SemiHandTrait_Fetch extends SessionContextSupport with ServletActorDyn {
+    
+/////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!!!! DON'T MOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///////////////////////////////    
+    val requestData = new DSRequestDyn(request)    
+    
+    logger debug s"Request for Fetch: ${newLine + requestData.toPrettyString}"    
+    
+    val dataSet = MvidDS(ds)    
+/////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!! END DON'T MOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///////////////////////////////    
+    
+
+}
