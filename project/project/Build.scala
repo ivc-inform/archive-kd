4277227dee3168524f48bb4990a7752274835cbe
andrew <uandrew1965@gmail.com>
2016-06-02 19:29:25 +0300

    .

--- project/project/Build.scala
+++ project/project/Build.scala
@@ -7,14 +7,14 @@ import sbt._
 
 object PluginsBuild extends Build
 {
-  //  val basePath = file(".")
-
   override def settings: Seq[Def.Setting[_]] = super.settings ++ CommonSettings.defaultSettings ++ Seq(
     git.baseVersion := CommonSettings.settingValues.baseVersion
   )
 
-  lazy val root = Project(id = "buildPlugins", base = file(".")).enablePlugins(GitVersioning).settings(
-    PluginDeps.devPlugin,
+  lazy val devPlugin = uri("../../build-plugins/dev-plugin")
+
+  lazy val root = Project(id = "buildPlugins", base = file(".")).enablePlugins(GitVersioning).dependsOn(devPlugin).settings(
+    //PluginDeps.devPlugin,
     PluginDeps.sourceGenJS,
     PluginDeps.transpileCoffeeScript,
     PluginDeps.mergeJS,
