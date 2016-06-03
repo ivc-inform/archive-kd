2e450e290418b5f7d2b36ae7e0bfce4a5ab3157d
andrew <uandrew1965@gmail.com>
2016-06-03 13:52:01 +0300

    .

---
+++ web-ui/src/main/scala/com/simplesys/js/components/EaKdProcWindowMain.scala
@@ -0,0 +1,10 @@
+package com.simplesys.js.components
+
+import com.simplesys.SmartClient.Foundation.Canvas
+import com.simplesys.js.com.simplesys.SmartClient.App.WebApp
+
+class EaKdProcWindowMain extends WebApp {
+
+    override protected def mainCanvas: Canvas =
+
+}
