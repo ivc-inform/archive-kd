ba0c3218b6977a9508a78befd72e553442e49ebc
andrew <uandrew1965@gmail.com>
2016-06-06 18:18:20 +0300

    .

---
+++ web-ui/src/main/scala/com/simplesys/container/SemiHandTraits/eakd_docizvtype_SemiHandTrait_Remove.scala
@@ -0,0 +1,23 @@
+// This file is generated automatically (at 06.06.2016 18:09:27), do not spend any changes here, because they will be lost. Generator: "GenBOContainer, stage: #765"
+
+package ru.simplesys.defs.app.scala.container
+
+import com.simplesys.app.SessionContextSupport
+import com.simplesys.isc.system.ServletActorDyn
+import com.simplesys.isc.dataBinging.DSRequestDyn
+import com.simplesys.common.Strings._
+import akka.actor.Actor
+import ru.simplesys.defs.bo.eakd._
+
+trait eakd_docizvtype_SemiHandTrait_Remove extends SessionContextSupport with ServletActorDyn {
+    
+/////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!!!! DON'T MOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///////////////////////////////    
+    val requestData = new DSRequestDyn(request)    
+    
+    logger debug s"Request for Remove: ${newLine + requestData.toPrettyString}"    
+    
+    val dataSet = DocizvtypeDS(ds)    
+/////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!! END DON'T MOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///////////////////////////////    
+    
+    def receiveBase: Option[Actor.Receive] = None
+}
---
+++ web-ui/src/main/scala/com/simplesys/container/SemiHandTraits/eakd_docrass_SemiHandTrait_Fetch.scala
@@ -0,0 +1,23 @@
+// This file is generated automatically (at 06.06.2016 18:09:27), do not spend any changes here, because they will be lost. Generator: "GenBOContainer, stage: #765"
+
+package ru.simplesys.defs.app.scala.container
+
+import com.simplesys.app.SessionContextSupport
+import com.simplesys.isc.system.ServletActorDyn
+import com.simplesys.isc.dataBinging.DSRequestDyn
+import com.simplesys.common.Strings._
+import akka.actor.Actor
+import ru.simplesys.defs.bo.eakd._
+
+trait eakd_docrass_SemiHandTrait_Fetch extends SessionContextSupport with ServletActorDyn {
+    
+/////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!!!! DON'T MOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///////////////////////////////    
+    val requestData = new DSRequestDyn(request)    
+    
+    logger debug s"Request for Fetch: ${newLine + requestData.toPrettyString}"    
+    
+    val dataSet = DocrassDS(ds)    
+/////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!! END DON'T MOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///////////////////////////////    
+    
+    def receiveBase: Option[Actor.Receive] = None
+}
