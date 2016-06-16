d2c317dd258ab6cc5e7fa388492ef72b615a64ee
andrew <uandrew1965@gmail.com>
2016-06-16 10:20:41 +0300

    .

---
+++ web-ui/src/main/scala/com/simplesys/refs/AbonentsOrg.scala
@@ -0,0 +1,16 @@
+package com.simplesys.refs
+
+import com.simplesys.SmartClient.Grids.ListGridEditor
+
+import scala.scalajs.js
+import scala.scalajs.js.|
+
+@js.native
+trait AbonentsOrg extends ListGridEditor {
+}
+
+//@js.native
+//abstract trait AbstractAbonentsOrgCompanion extends AbstractListGridEditorCompanion {
+//}
+
+
---
+++ web-ui/src/main/scala/com/simplesys/refs/props/AbonentsOrgProps.scala
@@ -0,0 +1,12 @@
+package com.simplesys.refs.props
+
+import com.simplesys.SmartClient.Grids.props.ListGridEditorProps
+import com.simplesys.SmartClient.System._
+import com.simplesys.System._
+import com.simplesys.function._
+import com.simplesys.option.ScOption._
+
+import scala.scalajs.js._
+
+class AbonentsOrgProps extends ListGridEditorProps {
+}
