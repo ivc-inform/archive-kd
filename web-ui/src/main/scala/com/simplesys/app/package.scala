69fb0fc18961ea187a2b605ccaef0633a25356ec
andrew <uandrew1965@gmail.com>
2016-06-16 13:28:35 +0300

    .

--- web-ui/src/main/scala/com/simplesys/app/package.scala
+++ web-ui/src/main/scala/com/simplesys/app/package.scala
@@ -3,9 +3,13 @@ package com.simplesys
 import com.simplesys.SmartClient.System.SCApply
 import com.simplesys.js.components.CommonListGridEditorComponent
 import com.simplesys.js.components.props.CommonListGridEditorComponentProps
+import com.simplesys.js.components.refs.AbonentsOrg
+import com.simplesys.js.components.refs.props.AbonentsOrgProps
 
+//Должны быть
 //import com.simplesys.SmartClient.System._
 //import com.simplesys.macros.PropsToDictionary
+//
 
 import com.simplesys.SmartClient.System._
 import com.simplesys.macros.PropsToDictionary
@@ -13,5 +17,5 @@ import com.simplesys.macros.PropsToDictionary
 package object app {
     val loadSchemas = false
     object CommonListGridEditorComponent extends SCApply[CommonListGridEditorComponent, CommonListGridEditorComponentProps]
-    //    object AbonentsOrg extends SCApply[AbonentsOrg, AbonentsOrgProps]
+    object AbonentsOrg extends SCApply[AbonentsOrg, AbonentsOrgProps]
 }
--- web-ui/src/main/scala/com/simplesys/js/components/props/CommonListGridEditorComponentProps.scala
+++ web-ui/src/main/scala/com/simplesys/js/components/props/CommonListGridEditorComponentProps.scala
@@ -6,10 +6,10 @@ import com.simplesys.System.Types.SelectionStyle
 import com.simplesys.option.ScOption._
 
 class CommonListGridEditorComponentProps extends ListGridEditorProps {
-//    drawAheadRatio = simpleSyS.config.drawAheadRatio.getOrElse(2.0).opt
-//    dataPageSize = simpleSyS.config.dataPageSize.getOrElse(75).opt
-//    canEdit = true.opt
-//    filterOnKeypress = true.opt
-//    autoFetchData = false.opt
-//    selectionType = SelectionStyle.multiple.opt
+    drawAheadRatio = simpleSyS.config.drawAheadRatio.getOrElse(2.0).opt
+    dataPageSize = simpleSyS.config.dataPageSize.getOrElse(75).opt
+    canEdit = true.opt
+    filterOnKeypress = true.opt
+    autoFetchData = false.opt
+    selectionType = SelectionStyle.multiple.opt
 }
