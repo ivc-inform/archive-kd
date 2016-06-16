c111571946d6ba561034efc8134daa443551ec88
andrew <uandrew1965@gmail.com>
2016-06-16 12:36:32 +0300

    .

--- web-ui/src/main/scala/com/simplesys/js/components/refs/props/CommonListGridEditorComponentProps.scala
+++ web-ui/src/main/scala/com/simplesys/js/components/refs/props/CommonListGridEditorComponentProps.scala
@@ -3,12 +3,7 @@ package com.simplesys.js.components.refs.props
 import com.simplesys.SmartClient.Grids.props.ListGridEditorProps
 import com.simplesys.SmartClient.System._
 import com.simplesys.System.Types.SelectionStyle
-import com.simplesys.System.Types.SelectionStyle._
-import com.simplesys.System._
-import com.simplesys.function._
 import com.simplesys.option.ScOption._
-
-import scala.scalajs.js._
 
 class CommonListGridEditorComponentProps extends ListGridEditorProps {
     drawAheadRatio = simpleSyS.config.drawAheadRatio.getOrElse(2.0).opt
