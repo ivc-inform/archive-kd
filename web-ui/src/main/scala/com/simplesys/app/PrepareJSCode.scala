e9d186712300262f846c20366762bd2d130677a0
andrew <uandrew1965@gmail.com>
2016-06-16 12:30:25 +0300

    .

--- web-ui/src/main/scala/com/simplesys/app/PrepareJSCode.scala
+++ web-ui/src/main/scala/com/simplesys/app/PrepareJSCode.scala
@@ -1,8 +1,6 @@
 package com.simplesys.app
 
 import com.simplesys.SmartClient.App.StaticJSCode
-import com.simplesys.SmartClient.System.{ListGridEditor, RibbonGroup, isc}
-import com.simplesys.js.components.{AbonentsOrg, CommonListGridEditorComponent}
 
 import scala.scalajs.js.annotation.JSExport
 
--- web-ui/src/main/scala/com/simplesys/js/components/package.scala
+++ web-ui/src/main/scala/com/simplesys/js/components/package.scala
@@ -1,11 +1,6 @@
 package com.simplesys.js
 
-import com.simplesys.SmartClient.System.SCApply
-import com.simplesys.js.components.refs.AbonentsOrg
-import com.simplesys.js.components.refs.props.{AbonentsOrgProps, CommonListGridEditorComponentProps}
-
 //import com.simplesys.macros.PropsToDictionary
-import com.simplesys.macros.PropsToDictionary
 
 package object components {
 //    object CommonListGridEditorComponent extends SCApply[CommonListGridEditorComponent, CommonListGridEditorComponentProps]
