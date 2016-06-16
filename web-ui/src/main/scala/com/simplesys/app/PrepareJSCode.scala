9aa2b15f989dfa198f133ee651575d006fc2ba1a
andrew <uandrew1965@gmail.com>
2016-06-16 15:43:55 +0300

    .

--- web-ui/src/main/scala/com/simplesys/app/PrepareJSCode.scala
+++ web-ui/src/main/scala/com/simplesys/app/PrepareJSCode.scala
@@ -1,6 +1,7 @@
 package com.simplesys.app
 
 import com.simplesys.SmartClient.App.StaticJSCode
+import com.simplesys.SmartClient.System.{ListGridEditor, isc}
 
 import scala.scalajs.js.annotation.JSExport
 
@@ -9,7 +10,7 @@ class PrepareJSCode extends StaticJSCode{
 
     @JSExport
     override def createJS(): Unit = {
-        //isc.defineClass(CommonListGridEditorComponent.getClass.getSimpleName, ListGridEditor.getClass.getSimpleName)
-        //isc.defineClass(AbonentsOrg.getClass.getSimpleName, CommonListGridEditorComponent.getClass.getSimpleName)
+        isc.defineClass(CommonListGridEditorComponent.getClass.getSimpleName, ListGridEditor.getClass.getSimpleName)
+        isc.defineClass(AbonentsOrg.getClass.getSimpleName, CommonListGridEditorComponent.getClass.getSimpleName)
     }
 }
