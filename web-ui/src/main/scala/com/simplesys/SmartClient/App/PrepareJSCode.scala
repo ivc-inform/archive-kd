3356e946db0fb80c2e11d685f8a507aa1ea3326c
andrew <uandrew1965@gmail.com>
2016-06-16 18:31:34 +0300

    .

--- web-ui/src/main/scala/com/simplesys/SmartClient/App/PrepareJSCode.scala
+++ web-ui/src/main/scala/com/simplesys/SmartClient/App/PrepareJSCode.scala
@@ -1,6 +1,6 @@
 package com.simplesys.SmartClient.App
 
-import com.simplesys.SmartClient.System.{Editoradmin_User, ListGridContextMenuWithInlineNewEditor, MenuSS, RibbonGroup, RibbonGroupSS, SettingsEditor, TreeGridContextMenu, TreeListGridEditor, User_ComponentMenu, WindowSS, isc}
+import com.simplesys.SmartClient.System.{Editoradmin_User, ListGridContextMenuWithFormNewEditor, ListGridContextMenuWithInlineNewEditor, MenuSS, RibbonGroup, RibbonGroupSS, SettingsEditor, TreeGridContextMenu, TreeListGridEditor, User_ComponentMenu, WindowSS, isc}
 
 import scala.scalajs.js.annotation.JSExport
 
@@ -12,6 +12,7 @@ class PrepareJSCode extends StaticJSCode {
         isc.defineClass(RibbonGroupSS.getClass.getSimpleName, RibbonGroup.getClass.getSimpleName)
         isc.defineClass(SettingsEditor.getClass.getSimpleName, WindowSS.getClass.getSimpleName)
         isc.defineClass(ListGridContextMenuWithInlineNewEditor.getClass.getSimpleName, MenuSS.getClass.getSimpleName)
+        isc.defineClass(ListGridContextMenuWithFormNewEditor.getClass.getSimpleName, MenuSS.getClass.getSimpleName)
         isc.defineClass(TreeGridContextMenu.getClass.getSimpleName, MenuSS.getClass.getSimpleName)
         isc.defineClass(User_ComponentMenu.getClass.getSimpleName, MenuSS.getClass.getSimpleName)
         isc.defineClass(Editoradmin_User.getClass.getSimpleName, TreeListGridEditor.getClass.getSimpleName)
