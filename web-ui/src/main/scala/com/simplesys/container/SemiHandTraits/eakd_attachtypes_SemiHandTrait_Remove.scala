f586b192e8ef634600ac8c095243262a54a5354d
andrew <uandrew1965@gmail.com>
2016-06-16 10:17:28 +0300

    .

---
+++ web-ui/src/main/scala/com/simplesys/container/SemiHandTraits/eakd_attachtypes_SemiHandTrait_Remove.scala
@@ -0,0 +1,24 @@
+// This file is generated automatically (at 16.06.2016 09:34:32), do not spend any changes here, because they will be lost. Generator: "GenBOContainer, stage: #765"
+
+package ru.simplesys.defs.app.scala.container
+
+import com.simplesys.app.SessionContextSupport
+import com.simplesys.isc.system.ServletActorDyn
+import com.simplesys.isc.dataBinging.DSRequestDyn
+import com.simplesys.common.Strings._
+import akka.actor.Actor
+import ru.simplesys.defs.bo.eakd._
+
+ 
+trait eakd_attachtypes_SemiHandTrait_Remove extends SessionContextSupport with ServletActorDyn {
+    
+/////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!!!! DON'T MOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///////////////////////////////    
+    val requestData = new DSRequestDyn(request)    
+    
+    logger debug s"Request for Remove: ${newLine + requestData.toPrettyString}"    
+    
+    val dataSet = AttachtypesDS(ds)    
+/////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!! END DON'T MOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///////////////////////////////    
+    
+     def receiveBase: Option[Actor.Receive] = None
+}
---
+++ web-ui/src/main/scala/com/simplesys/container/SemiHandTraits/eakd_docizvstat_SemiHandTrait_Add.scala
@@ -0,0 +1,24 @@
+// This file is generated automatically (at 16.06.2016 09:34:32), do not spend any changes here, because they will be lost. Generator: "GenBOContainer, stage: #765"
+
+package ru.simplesys.defs.app.scala.container
+
+import com.simplesys.app.SessionContextSupport
+import com.simplesys.isc.system.ServletActorDyn
+import com.simplesys.isc.dataBinging.DSRequestDyn
+import com.simplesys.common.Strings._
+import akka.actor.Actor
+import ru.simplesys.defs.bo.eakd._
+
+ 
+trait eakd_docizvstat_SemiHandTrait_Add extends SessionContextSupport with ServletActorDyn {
+    
+/////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!!!! DON'T MOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///////////////////////////////    
+    val requestData = new DSRequestDyn(request)    
+    
+    logger debug s"Request for Add: ${newLine + requestData.toPrettyString}"    
+    
+    val dataSet = DocizvstatDS(ds)    
+/////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!! END DON'T MOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///////////////////////////////    
+    
+     def receiveBase: Option[Actor.Receive] = None
+}
