cc62bdefd9ece75650016804058c35461f85afa4
andrew <uandrew1965@gmail.com>
2016-06-22 16:44:35 +0300

    .

--- web-ui/src/main/scala/com/simplesys/js/components/refs/props/GroupItemProps.scala
+++ web-ui/src/main/scala/com/simplesys/js/components/refs/props/GroupItemProps.scala
@@ -1,14 +1,14 @@
 package com.simplesys.js.components.refs.props
 
 import com.simplesys.SmartClient.App.props.CommonListGridEditorComponentProps
-import com.simplesys.js.components.refs.{GroupItem, MType}
+import com.simplesys.js.components.refs.GroupItem
 import com.simplesys.option.ScOption._
 import ru.simplesys.defs.app.gen.scala.ScalaJSGen.DataSourcesJS
 
 class GroupItemProps extends CommonListGridEditorComponentProps {
     override val simpleTable = true
 
-    type classHandler <: MType
+    type classHandler <: GroupItem
 
     dataSource = DataSourcesJS.eakd_mtype_DS.opt
     identifier = "D5151B68-E433-F7D0-BFA9-725DDECDAEDD".opt
