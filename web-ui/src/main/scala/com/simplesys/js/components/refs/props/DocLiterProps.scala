5537a99f2183115d288d33b8933e94c51938b6d3
andrew <uandrew1965@gmail.com>
2016-06-22 12:42:23 +0300

    .

--- web-ui/src/main/scala/com/simplesys/js/components/refs/props/DocLiterProps.scala
+++ web-ui/src/main/scala/com/simplesys/js/components/refs/props/DocLiterProps.scala
@@ -1,13 +1,14 @@
 package com.simplesys.js.components.refs.props
 
 import com.simplesys.SmartClient.App.props.CommonListGridEditorComponentProps
+import com.simplesys.js.components.refs.DocLiter
 import com.simplesys.option.ScOption._
 import ru.simplesys.defs.app.gen.scala.ScalaJSGen.DataSourcesJS
 
 class DocLiterProps extends CommonListGridEditorComponentProps {
     override val simpleTable = true
 
-    type classHandler <: DocLiterProps
+    type classHandler <: DocLiter
 
     dataSource = DataSourcesJS.eakd_docliter_DS.opt
     identifier = "D5DE1B68-E433-5220-BFA9-7BFDDECDAEDD".opt
