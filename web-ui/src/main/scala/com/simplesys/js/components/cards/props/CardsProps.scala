dfd9aa0ae10322c2a6c32a50d707e2928e076c4b
andrew <uandrew1965@gmail.com>
2016-06-23 16:24:36 +0300

    .

--- web-ui/src/main/scala/com/simplesys/js/components/cards/props/CardsProps.scala
+++ web-ui/src/main/scala/com/simplesys/js/components/cards/props/CardsProps.scala
@@ -1,15 +1,102 @@
 package com.simplesys.js.components.cards.props
 
 import com.simplesys.SmartClient.App.props.CommonListGridEditorComponentProps
+import com.simplesys.SmartClient.Forms.FormsItems.props.TextItemProps
+import com.simplesys.SmartClient.System._
+import com.simplesys.System.Types.FieldType
 import com.simplesys.js.components.cards.Cards
 import com.simplesys.option.ScOption._
 import ru.simplesys.defs.app.gen.scala.ScalaJSGen.DataSourcesJS
 
-class CardsProps extends CommonListGridEditorComponentProps {
-    override val simpleTable = true
+class CardsProps extends CommonListGridEditorComponentProps with Implicits {
+    override val simpleTable = false
 
     type classHandler <: Cards
 
     dataSource = DataSourcesJS.eakd_card_DS.opt
     identifier = "D8DC1876-F489-3172-EE97-729FFB73B575".opt
+    editingFields =
+      Seq(
+          TextItem(new TextItemProps {
+              name = "datecard"
+          }),
+          TextItem(new TextItemProps {
+              hidden = true
+              name = "idcats"
+          }),
+          TextItem(new TextItemProps {
+              name = "idcrd_fk"
+          }),
+          TextItem(new TextItemProps {
+              hidden = true
+              name = "iddocitem"
+          }),
+          TextItem(new TextItemProps {
+              hidden = true
+              name = "idgroup"
+          }),
+          TextItem(new TextItemProps {
+              hidden = true
+              name = "idliter"
+          }),
+          TextItem(new TextItemProps {
+              hidden = true
+              name = "idstate"
+          }),
+          TextItem(new TextItemProps {
+              hidden = true
+              name = "idtype"
+          }),
+          TextItem(new TextItemProps {
+              name = "vcrcode"
+          }),
+          TextItem(new TextItemProps {
+              name = "vcrcodeprns"
+          }),
+          TextItem(new TextItemProps {
+              name = "vcrinvent"
+          }),
+          TextItem(new TextItemProps {
+              name = "vcrkolpages"
+          }),
+          TextItem(new TextItemProps {
+              name = "vcrname"
+          }),
+          TextItem(new TextItemProps {
+              name = "vcrsdescr"
+          }),
+          TextItem(new TextItemProps {
+              name = "vfileident"
+          }),
+          TextItem(new TextItemProps {
+              name = "vformats"
+          }),
+          TextItem(new TextItemProps {
+              name = "vrazr"
+          }),
+          TextItem(new TextItemProps {
+              name = "vlrcode"
+          }),
+          TextItem(new TextItemProps {
+              name = "vlrname"
+          }),
+          TextItem(new TextItemProps {
+              name = "vtyname"
+          }),
+          TextItem(new TextItemProps {
+              name = "vtyname"
+          }),
+          TextItem(new TextItemProps {
+              name = "vctcode"
+          }),
+          TextItem(new TextItemProps {
+              name = "vsecode"
+          }),
+          TextItem(new TextItemProps {
+              name = "vgicode"
+          }),
+          TextItem(new TextItemProps {
+              name = "vitcode"
+          })
+      ).opt
 }
