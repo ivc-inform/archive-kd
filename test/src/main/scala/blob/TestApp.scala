74ecc47a43e3ee67b57eacf50d8ce534545b3b42
andrew <uandrew1965@gmail.com>
2017-05-18 18:23:09 +0300

    .

--- test/src/main/scala/blob/TestApp.scala
+++ test/src/main/scala/blob/TestApp.scala
@@ -3,15 +3,40 @@ package blob
 import java.io.{File, FileInputStream}
 
 import com.simplesys.connectionStack.BoneCPStack
-import com.simplesys.tuple.TupleSS2
-import oracle.jdbc.OraclePreparedStatement
-import org.joda.time.DateTime
-import org.joda.time.format.DateTimeFormat
-import ru.simplesys.defs.bo.test.table.{Upload_filesTbl, Upload_filesTblFile_content}
+
+object DT {
+    def apply(d: Long): DT = {
+        var seconds: Long = d / 1000
+        var minits = 0L
+        var hours = 0L
+        var days = 0L
+
+        if (seconds / 60 > 0) {
+            minits = seconds / 60
+            seconds -= minits * 60
+        }
+
+        if (minits / 60 > 0) {
+            hours = minits / 60
+            minits -= hours * 60
+        }
+
+        if (hours / 24 > 0) {
+            days = hours / 24
+            hours -= days * 24
+        }
+
+        DT(days, hours, minits, seconds)
+    }
+}
+
+case class DT(days: Long, hours: Long, minits: Long, seconds: Long) {
+    override def toString: String = s"Days: $days, Hours: $hours, Mins: $minits, Secs: $seconds"
+}
 
 object TestApp extends App with BoneCPStack {
     val ds = OracleDataSource("oracleEAKD")
-    
+
     //val upload_filesTbl = Upload_filesTbl(ds)
     //val upload_filesBo1 = Upload_filesTblFile_content(ds)
 
@@ -23,7 +48,8 @@ object TestApp extends App with BoneCPStack {
     //val fileName = "build.sbt"
 
     val file = new File(fileName)
-    val out = new FileInputStream(file)
+    val fin = new FileInputStream(file)
+    val fileContent = new Array[Byte](file.length.asInstanceOf[Int])
 
     val con = ds.Connection
     con setAutoCommit false
@@ -35,16 +61,21 @@ object TestApp extends App with BoneCPStack {
 
     val startTime = System.currentTimeMillis()
 
-    println(s"seting BinaryStream for file: $fileName")
-    
-    pstmt.setBlob(3, out)
+    fin.read(fileContent)
+    println(s"fin.read")
+
+    pstmt.setBytes(3, fileContent)
+    println(s"pstmt.setBytes")
+
     pstmt.executeUpdate()
+    println(s"pstmt.executeUpdate")
+
     con.commit()
+    println(s"con.commit")
 
     val elapsedTime = System.currentTimeMillis() - startTime
-    
-    println(s"elapsedTime : $elapsedTime ms")
 
+    println(s"elapsedTime : ${DT(elapsedTime).toString}")
     println("Test executed")
 
     //upload_filesBo1.updateP(Upload_filesFile_content(1L, "Red_Hot.mkv"))
