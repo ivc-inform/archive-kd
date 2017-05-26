288fd6ca0b96764aad5ea58282ce16cff1acb4cc
andrew <uandrew1965@gmail.com>
2017-05-26 17:21:23 +0300

    .

--- web-ui/src/main/scala/com/simplesys/SmartClient/Tools/palette/HiddenPalette.scala
+++ web-ui/src/main/scala/com/simplesys/SmartClient/Tools/palette/HiddenPalette.scala
@@ -4,8 +4,10 @@ import com.simplesys.SmartClient.System.IscArray
 import com.simplesys.SmartClient.Tools.{Palette, PaletteNode}
 
 import scala.scalajs.js
+import scala.scalajs.js.annotation.JSGlobal
 
 @js.native
+@JSGlobal
 trait HiddenPalette extends Palette {
     val data: IscArray[PaletteNode]
 }
