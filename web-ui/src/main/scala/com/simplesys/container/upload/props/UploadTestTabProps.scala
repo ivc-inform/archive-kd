883f9f91205eb29cda9d95db355755f5e93edf03
andrew <uandrew1965@gmail.com>
2017-06-29 18:18:55 +0300

    .

--- web-ui/src/main/scala/com/simplesys/container/upload/props/UploadTestTabProps.scala
+++ web-ui/src/main/scala/com/simplesys/container/upload/props/UploadTestTabProps.scala
@@ -1,5 +1,7 @@
 package com.simplesys.container.upload.props
 
+import com.simplesys.SmartClient.DataBinding.props.DataSourceSSProps
+import com.simplesys.SmartClient.DataBinding.props.dataSource.DataSourceFieldProps
 import com.simplesys.SmartClient.DataBinding.{DSRequest, DSResponse}
 import com.simplesys.SmartClient.Forms.DynamicFormSS
 import com.simplesys.SmartClient.Forms.formsItems.props.{ProgressbarItemProps, SubmitItemProps, UploadItemProps}
@@ -7,6 +9,7 @@ import com.simplesys.SmartClient.Forms.formsItems.{FormItem, UploadItem}
 import com.simplesys.SmartClient.Forms.props.DynamicFormSSProps
 import com.simplesys.SmartClient.Layout.props.HLayoutProps
 import com.simplesys.SmartClient.System._
+import com.simplesys.System.Types.FormItemComponentType
 import com.simplesys.System._
 import com.simplesys.container.upload.UploadTestTab
 import com.simplesys.function._
@@ -17,18 +20,28 @@ class UploadTestTabProps extends HLayoutProps {
     type classHandler <: UploadTestTab
 
     identifier = "69EC6EB4-E51F-B7A9-C1E0-CF216088816AF".opt
-    width = "100%"
 
     initWidget = {
         (thiz: classHandler, arguments: IscArray[JSAny]) =>
 
             thiz.Super("initWidget", arguments)
 
-            val form = DynamicFormSS.create(
+            thiz addMember DynamicFormSS.create(
                 new DynamicFormSSProps {
-                    width = "*"
+                    width = "100%"
                     //action = "UploadServlet".opt
                     //encoding = Encoding.multipart.opt
+                    dataSource = DataSourceSS.create(
+                        new DataSourceSSProps {
+                            fields = Seq(
+                                new DataSourceFieldProps {
+                                    name = "file".opt
+                                    showFileInline = true.opt
+                                    `type` = FormItemComponentType.FileItem
+                                }
+                            ).opt
+                        }
+                    ).opt
                     items = Seq(
                         UploadItem(
                             new UploadItemProps {
@@ -36,8 +49,6 @@ class UploadTestTabProps extends HLayoutProps {
                                 nameStrong = "file".nameStrongOpt
                                 showTitle = false.opt
                                 title = "Choose file".opt
-                                //showFileInline = true.opt
-                                ID = "69EC6EB4-E51F-B7A9-C1E0-CF216085122F".opt
                                 changed = {
                                     (form: DynamicFormSS, item: UploadItem, value: JSUndefined[JSAny]) ⇒
                                         val submit = form getItem "upload"
@@ -53,13 +64,13 @@ class UploadTestTabProps extends HLayoutProps {
                                 nameStrong = "upload".nameStrongOpt
                                 click = {
                                     (form: DynamicFormSS, item: FormItem) ⇒
-                                        /*form.saveData(
+                                        form.saveData(
                                             {
                                                 (response: DSResponse, obj: JSObject, request: DSRequest) ⇒
                                                     if (response.status == DSResponse.STATUS_SUCCESS)
                                                         isc ok "OK"
                                             }.toFunc
-                                        )*/
+                                        )
                                         false
                                 }.toFunc.opt
                             }
@@ -67,14 +78,11 @@ class UploadTestTabProps extends HLayoutProps {
                         ProgressbarItem(
                             new ProgressbarItemProps {
                                 title = "Процесс выгрузки".ellipsis.opt
-                                width = "*"
                             }
                         )
                     ).opt
                 }
             )
-
-            thiz addMember form
 
         //isc debugTrac (thiz.getClassName(), thiz.getIdentifier())
 
