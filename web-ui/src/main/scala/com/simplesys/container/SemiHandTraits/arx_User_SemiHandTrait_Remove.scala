6c8d5bbe13d8ec9becd01163c2569e0bd4f6324e
andrew <uandrew1965@gmail.com>
2017-07-14 11:17:30 +0300

    .

---
+++ web-ui/src/main/scala/com/simplesys/container/SemiHandTraits/arx_User_SemiHandTrait_Remove.scala
@@ -0,0 +1,27 @@
+// This file is generated automatically (at 13.07.2017 18:43:23), do not spend any changes here, because they will be lost. Generator: "GenBOContainer, stage: #765"
+
+package ru.simplesys.defs.app.scala.container
+
+import com.simplesys.app.SessionContextSupport
+import com.simplesys.isc.system.ServletActorDyn
+import com.simplesys.isc.dataBinging.DSRequestDyn
+import com.simplesys.common.Strings._
+import com.simplesys.jdbc.control.clob._
+import akka.actor.Actor
+import ru.simplesys.defs.bo.arx._
+
+ 
+trait arx_User_SemiHandTrait_Remove extends SessionContextSupport with ServletActorDyn {
+    
+/////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!!!! DON'T MOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///////////////////////////////    
+    val requestData = new DSRequestDyn(request)    
+    
+    logger debug s"Request for Remove: ${newLine + requestData.toPrettyString}"    
+    
+    val dataSet = UserDS(ds)    
+/////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!! END DON'T MOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///////////////////////////////    
+    
+     def receiveBase: Option[Actor.Receive] = None    
+    
+     def wrapperBlobGetter(blob: Blob): String = blob.asString
+}
