53b47ed88b26735e0dda7512c0026a813fb00cc1
andrew <uandrew1965@gmail.com>
2017-07-19 10:29:01 +0300

    .

--- web-ui/src/main/scala/com/simplesys/js/components/cards/props/CardsProps.scala
+++ web-ui/src/main/scala/com/simplesys/js/components/cards/props/CardsProps.scala
@@ -1,9 +1,13 @@
 package com.simplesys.js.components.cards.props
 
 import com.simplesys.SmartClient.App.props._
+import com.simplesys.SmartClient.Control.MenuSS
+import com.simplesys.SmartClient.Control.menu.MenuSSItem
 import com.simplesys.SmartClient.Control.props.MenuSSProps
+import com.simplesys.SmartClient.Control.props.menu.MenuSSItemProps
 import com.simplesys.SmartClient.Foundation.Canvas
 import com.simplesys.SmartClient.Foundation.props.CanvasProps
+import com.simplesys.SmartClient.Grids.ListGridEditor
 import com.simplesys.SmartClient.Grids.listGrid.ListGridRecord
 import com.simplesys.SmartClient.Layout.props.WindowSSProps
 import com.simplesys.SmartClient.System._
@@ -40,10 +44,52 @@ class CardsProps extends CommonListGridEditorComponentProps with Implicits {
         (thiz: classHandler, component: JSUndefined[Canvas], record: ListGridRecord) ⇒
             val menu = MenuSS.create(
                 new MenuSSProps {
-                    
+                    items = Seq(
+                        new MenuSSItemProps {
+                            title = "Вложения".ellipsis.opt
+                            identifier = "attachments".opt
+                            icon = Common.attach.opt
+                            click = {
+                                (target: Canvas, item: MenuSSItem, menu: MenuSS, colNum: JSUndefined[Int]) =>
+                                    thiz.Super("expandRecord", IscArray(component, record))
+                                    false
+                            }.toFunc.opt
+                            enableIf = {
+                                (target: Canvas, menu: MenuSS, item: MenuSSItem) =>
+                                    true
+                            }.toFunc.opt
+                        },
+                        new MenuSSItemProps {
+                            title = "Рассылка".ellipsis.opt
+                            identifier = "distribution".opt
+                            icon = Common.distribution.opt
+                            click = {
+                                (target: Canvas, item: MenuSSItem, menu: MenuSS, colNum: JSUndefined[Int]) =>
+
+                                    false
+                            }.toFunc.opt
+                            enableIf = {
+                                (target: Canvas, menu: MenuSS, item: MenuSSItem) =>
+                                    false
+                            }.toFunc.opt
+                        },
+                        new MenuSSItemProps {
+                            title = "Запросы".ellipsis.opt
+                            identifier = "requests".opt
+                            icon = Common.request.opt
+                            click = {
+                                (target: Canvas, item: MenuSSItem, menu: MenuSS, colNum: JSUndefined[Int]) =>
+
+                                    false
+                            }.toFunc.opt
+                            enableIf = {
+                                (target: Canvas, menu: MenuSS, item: MenuSSItem) =>
+                                    false
+                            }.toFunc.opt
+                        }
+                    ).opt
                 }
             )
-            thiz.Super("expandRecord", IscArray(component, record))
     }.toThisFunc.opt
 
     getExpansionComponent = {
