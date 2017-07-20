ddc83b1751e5c73a5de4ed236cb1687d79737ddc
andrew <uandrew1965@gmail.com>
2017-07-20 14:26:36 +0300

    .

--- web-ui/src/main/scala/com/simplesys/container/SemiHandTraits/arx_attatch_SemiHandTrait_Fetch.scala
+++ web-ui/src/main/scala/com/simplesys/container/SemiHandTraits/arx_attatch_SemiHandTrait_Fetch.scala
@@ -18,7 +18,7 @@ import com.simplesys.servlet.GetData
 import com.simplesys.tuple.TupleSS14
 import org.joda.time.LocalDateTime
 import ru.simplesys.defs.app.gen.scala.ScalaJSGen._
-import ru.simplesys.defs.bo.arx.{AttatchDS, DocizvstatDS, DocizvtypeDS}
+import ru.simplesys.defs.bo.arx.{AttatchDS, DocizvDS, DocizvstatDS, DocizvtypeDS}
 
 import scalaz.{Failure, Success}
 
@@ -33,6 +33,7 @@ trait arx_attatch_SemiHandTrait_Fetch extends SessionContextSupport with Servlet
     val dataSet = AttatchDS(ds)
     /////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!! END DON'T MOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///////////////////////////////
 
+    val dataSetDocIzv = DocizvDS(ds)
     val dataSetDocIzvStat = DocizvstatDS(ds)
     val dataSetDocIzvType = DocizvtypeDS(ds)
 
@@ -68,26 +69,29 @@ trait arx_attatch_SemiHandTrait_Fetch extends SessionContextSupport with Servlet
                             idDocizv: Long,
                             vizcodeDocizv: Array[String]) ⇒
 
-                                dataSetDocIzvStat.selectPList(where = Where(dataSetDocIzvStat.idDocizvstat === idDocizv)) result match {
+                                _data += RecordDyn(
+                                    arx_attatch_id_NameStrong.name → idAttatch,
+                                    arx_attatch_vatcode_NameStrong.name → vatcodeAttatch,
+                                    arx_attatch_ddatein_NameStrong.name → ddateinAttatch,
+                                    arx_attatch_vatdescr_NameStrong.name → vatdescrAttatch,
+                                    arx_attatch_invnumenz_NameStrong.name → invnumenzAttatch,
+                                    arx_attatch_idizv_NameStrong.name → idizvAttatch,
+                                    arx_attatch_idattypes_NameStrong.name → idattypesAttatch,
+                                    arx_attatch_idcard_NameStrong.name → idcardAttatch,
+                                    arx_attatch_vizcode_NameStrong.name → vizcodeDocizv,
+                                    arx_attatch_vattypename_NameStrong.name → vattypenameAttatchtypes,
+                                    arx_attatch_vcrcode_NameStrong.name → vcrcodeCard /*,
+                                                                            arx_docizvstat_vname_NameStrong.name → vnameDocizvstat*/
+                                )
+
+                                logger debug s"idDocizv: $idDocizv"
+                                /*dataSetDocIzv.selectPList(where = Where(dataSetDocIzv.idDocizv === idDocizv)) result match {
                                     case Success(item) ⇒
-                                        _data += RecordDyn(
-                                            arx_attatch_id_NameStrong.name → idAttatch,
-                                            arx_attatch_vatcode_NameStrong.name → vatcodeAttatch,
-                                            arx_attatch_ddatein_NameStrong.name → ddateinAttatch,
-                                            arx_attatch_vatdescr_NameStrong.name → vatdescrAttatch,
-                                            arx_attatch_invnumenz_NameStrong.name → invnumenzAttatch,
-                                            arx_attatch_idizv_NameStrong.name → idizvAttatch,
-                                            arx_attatch_idattypes_NameStrong.name → idattypesAttatch,
-                                            arx_attatch_idcard_NameStrong.name → idcardAttatch,
-                                            arx_attatch_vizcode_NameStrong.name → vizcodeDocizv,
-                                            arx_attatch_vattypename_NameStrong.name → vattypenameAttatchtypes,
-                                            arx_attatch_vcrcode_NameStrong.name → vcrcodeCard,
-                                            arx_docizvstat_vname_NameStrong.name → item.vnameDocizvstat
-                                        )
+
 
                                     case x =>
                                         new RuntimeException(s"mached as : $x")
-                                }
+                                }*/
                             case x =>
                                 new RuntimeException(s"mached as : $x")
                         }
