39b610b55909262777b6c4b87fe50387abf35e42
andrew <uandrew1965@gmail.com>
2017-07-25 13:57:44 +0300

    .

--- web-ui/src/main/scala/com/simplesys/SmartClient/Forms/formsItems/props/FormItemProps.scala
+++ web-ui/src/main/scala/com/simplesys/SmartClient/Forms/formsItems/props/FormItemProps.scala
@@ -130,7 +130,7 @@ class FormItemProps extends ClassProps {
     var loadingDisplayValue: ScOption[String] = ScNone
     var locateItemBy: ScOption[String] = ScNone
     var multipleValueSeparator: ScOption[String] = ScNone
-    @deprecated(message = "Use nameStrong instead.", since = "")
+    //@deprecated(message = "Use nameStrong instead.", "")
     var name: ScOption[String] = ScNone
     @JSName("name")
     var _name: ScOption[String] = ScNone
