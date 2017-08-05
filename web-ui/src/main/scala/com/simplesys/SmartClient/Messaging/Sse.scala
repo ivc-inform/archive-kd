ec8cf1e6ad25433bfa15bf842289156ec1d40205
andrew <uandrew1965@gmail.com>
2017-08-05 15:47:07 +0300

    .

--- web-ui/src/main/scala/com/simplesys/SmartClient/Messaging/Sse.scala
+++ web-ui/src/main/scala/com/simplesys/SmartClient/Messaging/Sse.scala
@@ -1,7 +1,7 @@
 package com.simplesys.SmartClient.Messaging
 
 import com.simplesys.SmartClient.System.{AbstractClassCompanion, IscArray, isc}
-import com.simplesys.System.JSObject
+import com.simplesys.System.{JSArray, JSObject}
 import org.scalajs.dom.raw.{EventSource, MessageEvent}
 import org.scalajs.dom.window
 
@@ -14,17 +14,15 @@ import com.simplesys.System.Types.Callback
 
 trait Channel extends JSObject {
     val callback: SseCallBack
+    val event: Option[String]
 }
 
-class Sse extends JSObject
-
-object Sse {
+class Sse extends JSObject {
     type SseCallBack = js.Function1[MessageEvent, _]
 
-    private val channels = Dictionary.empty[SseCallBack]
+    private val channels = Dictionary.empty[Channel]
 
-    @JSExportStatic
-    def checkExistsSSE(): Boolean = {
+    private def checkExistsSSE(): Boolean = {
         if (!window.hasOwnProperty("EventSource")) {
             isc error ("Ваш браузер не поддерживает технологию SSE, что делает невозможным автоматическое получение сообщений от сервера. (Данная задача находится в доработке.)")
             false
@@ -32,11 +30,28 @@ object Sse {
             true
     }
 
+    private def reconnect(): Unit = {
 
-    def subscribe(channel: String, callback: SseCallBack, subscribeCallback: Option[Callback] = None, event: Option[String] = None): Unit = {
-        //        val eventSource = new EventSource("")
-        //        eventSource.onmessage
     }
+
+
+    def subscribe(channels: IscArray[String], _callback: SseCallBack, subscribeCallback: Option[Callback] = None, _event: Option[String] = None, _reconnect: Boolean = true): Unit = {
+       channels.
+    }
+
+    def subscribe(channel: String, _callback: SseCallBack, subscribeCallback: Option[Callback] = None, _event: Option[String] = None, _reconnect: Boolean = true): Unit = {
+        channels(channel) = new Channel {
+            override val callback: SseCallBack = _callback
+            override val event: Option[String] = _event
+        }
+
+        if (_reconnect)
+            reconnect()
+    }
+}
+
+object Sse {
+    type SseCallBack = js.Function1[MessageEvent, _]
 }
 
 
