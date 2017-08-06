8a6729df686c49556cf2a12d14579147fea0650e
andrew <uandrew1965@gmail.com>
2017-08-06 16:24:31 +0300

    .

--- web-ui/src/main/scala/com/simplesys/SmartClient/System/URIBuilder.scala
+++ web-ui/src/main/scala/com/simplesys/SmartClient/System/URIBuilder.scala
@@ -1,13 +1,19 @@
 package com.simplesys.SmartClient.System
 
 
-import com.simplesys.System.Types.URL
+import com.simplesys.System.Types.{URL, void}
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSGlobal
 
 @js.native
 trait URIBuilder extends Class {
+    def init(): void
+    def appendPath(path: String): void
+    def containsQueryParam(name: String): Boolean
+    def getQueryValue[T](paramName: String, firstOnly: Boolean): T
+    def appendQueryParam[T](name: String, value: T): void
+    def setQueryParam[T](name: String, value: T): void
 }
 
 @js.native
