f5cc3a5d68eb9f29bdfc5adcfbf7cb2193e6e23c
andrew <uandrew1965@gmail.com>
2017-09-05 19:22:52 +0300

    .

--- project/CommonSettings.scala
+++ project/CommonSettings.scala
@@ -6,7 +6,7 @@ object CommonSettings {
   object settingValues {
 
     val scalaVersion = "2.12.3"
-    val organization = "com.ivc-inform.archive"
+    val organization = "com.ivc-inform"
     val name = "archive-kd"
     val version = "1.0.0.1"
     val scalacOptions = Seq(
