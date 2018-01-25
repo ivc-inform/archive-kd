c738e2eaf742129e7da59b14b137cda18757add8
andrew <uandrew1965@gmail.com>
2018-01-25 17:05:27 +0300

    .

--- src/main/docker/11.2.0.1.0_32/build.sh
+++
@@ -1,3 +0,0 @@
-docker build --no-cache --force-rm --tag ivcinform/oracle-11x32g:1.0.0 .
-#docker build --no-cache --force-rm --tag ivcinform/centos-i386:centos6 .
-#docker run -it --name ora_1132 ivcinform/oracle-11x32g:1.0.0 bash
